e0d06b5b86fa0bcae8589fd337edab192c0f057a
Nadim <nadimabrar@gmail.com>
2021-03-31 13:40:28 +0600

    Add decoded transaction and also add a form to broadcast signed transaction.

--- components/Form.vue
+++ components/Form.vue
@@ -1,89 +1,102 @@
 <template>
   <div>
-    <section v-if="showForm">
-      <div class="has-text-centered">
-        <h3 class="is-size-5"> <b>Create Signed Transaction Offline</b> </h3>
-      </div>
-      <br>
-      <form @submit.prevent="createTransaction">
-        <b-field label="Genesis Hash">
-            <b-input v-model="formData.genesisHash" :lazy="true"></b-input>
-        </b-field>
+    <b-tabs type="is-boxed">
+      <b-tab-item label="Create Signed Transaction Offline" icon="plus">
+        <section v-if="showForm">
+          <form @submit.prevent="createTransaction">
+            <b-field label="Genesis Hash">
+                <b-input v-model="formData.genesisHash" :lazy="true"></b-input>
+            </b-field>
 
-        <b-field label="RPC Metadata">
-            <b-input type="textarea" v-model="formData.metadata" :lazy="true"></b-input>
-        </b-field>
+            <b-field label="RPC Metadata">
+                <b-input type="textarea" v-model="formData.metadata" :lazy="true"></b-input>
+            </b-field>
 
-        <b-field label="Spec Version">
-            <b-input v-model="formData.specVersion" :lazy="true"></b-input>
-        </b-field>
+            <b-field label="Spec Version">
+                <b-input v-model="formData.specVersion" :lazy="true"></b-input>
+            </b-field>
 
-        <b-field label="Transaction Version">
-            <b-input v-model="formData.transactionVersion" :lazy="true"></b-input>
-        </b-field>
+            <b-field label="Transaction Version">
+                <b-input v-model="formData.transactionVersion" :lazy="true"></b-input>
+            </b-field>
 
-        <b-field label="Destination Address">
-            <b-input v-model="formData.destination" :lazy="true"></b-input>
-        </b-field>
+            <b-field label="Destination Address">
+                <b-input v-model="formData.destination" :lazy="true"></b-input>
+            </b-field>
 
-        <b-field label="Amount">
-            <b-input v-model="formData.amount" type="number" min="1" :lazy="true"></b-input>
-        </b-field>
+            <b-field label="Amount">
+                <b-input v-model="formData.amount" type="number" min="1" :lazy="true"></b-input>
+            </b-field>
 
-        <b-field label="Nonce">
-            <b-input v-model="formData.nonce" type="number" min="0" :lazy="true"></b-input>
-        </b-field>
+            <b-field label="Nonce">
+                <b-input v-model="formData.nonce" type="number" min="0" :lazy="true"></b-input>
+            </b-field>
 
-        <b-field label="Tip">
-            <b-input v-model="formData.tip" type="number" min="0" :lazy="true"></b-input>
-        </b-field>
+            <b-field label="Tip">
+                <b-input v-model="formData.tip" type="number" min="0" :lazy="true"></b-input>
+            </b-field>
 
-        <br>
+            <br>
 
-        <FileReader @load="loadFileDataToMnemonic($event)"></FileReader>
+            <FileReader @load="loadFileDataToMnemonic($event)"></FileReader>
 
-        <br>
+            <br>
 
-        <div class="has-text-centered">
-          <b-button
-            label="Create Transaction"
-            native-type="submit"
-            class="is-primary" expanded/>
-        </div>
-      </form>
-    </section>
-    <section v-else>
-      <h3 class="is-size-6"> <b>Unsigned Transaction</b> </h3>
-      <pre>{{JSON.stringify(txData.unsignedTx, undefined, 2)}}</pre>
-      <br>
-      <h3 class="is-size-6"> <b>Signning Payload</b> </h3>
-      <pre class="wrapword">{{txData.signingPayload}}</pre>
-      <br>
-      <h3 class="is-size-6"> <b>Signature</b> </h3>
-      <pre class="wrapword">{{txData.signature}}</pre>
-      <br>
-      <h3 class="is-size-6"> <b>Signed Transaction</b> </h3>
-      <pre class="wrapword">{{txData.signedTx}}</pre>
-      <br>
-      <h3 class="is-size-6"> <b>Expected Transaction Hash</b> </h3>
-      <pre class="wrapword">{{txData.expectedTxHash}}</pre>
-      <br>
+            <div class="has-text-centered">
+              <b-button
+                label="Create Transaction"
+                native-type="submit"
+                class="is-primary" expanded/>
+            </div>
+          </form>
+        </section>
+        <section v-else>
+          <h3 class="is-size-6"> <b>Unsigned Transaction</b> </h3>
+          <pre>{{txData.unsignedTx}}</pre>
+          <br>
+          <h3 class="is-size-6"> <b>Decoded Unsigned Transaction</b> </h3>
+          <pre>{{txData.decodedUnsignedTx}}</pre>
+          <br>
+          <h3 class="is-size-6"> <b>Signning Payload</b> </h3>
+          <pre class="wrapword">{{txData.signingPayload}}</pre>
+          <br>
+          <h3 class="is-size-6"> <b>Signature</b> </h3>
+          <pre class="wrapword">{{txData.signature}}</pre>
+          <br>
+          <h3 class="is-size-6"> <b>Signed Transaction</b> </h3>
+          <pre class="wrapword">{{txData.signedTx}}</pre>
+          <br>
+          <h3 class="is-size-6"> <b>Expected Transaction Hash</b> </h3>
+          <pre class="wrapword">{{txData.expectedTxHash}}</pre>
+          <br>
+          
+          <div class="has-text-centered">
+            <b-button
+              @click="resetApp()"
+              label="Create New Transaction"
+              class="is-primary" expanded/>
+          </div>
+        </section>
+      </b-tab-item>
+      <b-tab-item label="Broadcast" icon="access-point">
+         <form @submit.prevent="submitTx()">
+            <b-field label="Signed Transaction">
+                <b-input type="textarea" v-model="broadcastData.signedTx" :lazy="true"></b-input>
+            </b-field>
+            <b-field label="URL to broadcast">
+                <b-input v-model="broadcastData.url" :lazy="true"></b-input>
+            </b-field>
+            <br>
 
-      <b-field label="URL to broadcast">
-          <b-input v-model="formData.url" :lazy="true"></b-input>
-      </b-field>
-
-      <div class="has-text-centered">
-        <b-button
-          @click="submitTx()"
-          label="Broadcast"
-          class="is-primary" expanded/>
-        <b-button
-          @click="resetApp()"
-          label="Create New Transaction"
-          class="is-primary is-light" expanded/>
-      </div>
-    </section>
+            <div class="has-text-centered">
+              <b-button
+                native-type="submit"
+                label="Broadcast"
+                class="is-primary" expanded/>
+            </div>
+          </form>
+      </b-tab-item>
+    </b-tabs>
   </div>
 </template>
 
@@ -102,13 +115,24 @@ import {
   createSigningPayload, 
   createSignedTx, 
   getTxHash, 
+  decode,
   POLKADOT_SS58_FORMAT,
 } from '@substrate/txwrapper'
+
+function decodePatch(unsignedTx, txOptions) {
+  let unsignedTx_ = { ...unsignedTx }
+  // This is an ugly hack to decode immortal transactions. Because the decoder provided in txWrapper
+  // always expects mortal era and fails to decode immortal era.
+  // See https://github.com/paritytech/txwrapper/blob/master/src/decode/decodeUnsignedTx.ts#L39
+  unsignedTx_.era = "0x0100"
+  let decodedUnsignedTx = decode(unsignedTx_, txOptions);
+  decodedUnsignedTx.eraPeriod = 0
+  return decodedUnsignedTx
+}
 
 const initialData = {
     formData: {
       // Chain / Node 
-      url: '',
       genesisHash: '',
       metadata: '',
       specVersion: '',
@@ -121,11 +145,16 @@ const initialData = {
       mnemonic: '',
     },
     txData: {
-      unsignedTx: {},
+      unsignedTx: "",
+      decodedUnsignedTx: "",
       signingPayload: '',
       signature: '',
       signedTx: '',
       expectedTxHash: ''
+    },
+    broadcastData: {
+      signedTx: '',
+      url: '',
     },
     showForm: true,
 }
@@ -168,12 +197,14 @@ export default {
       // This is needed since the transferKeepAlive library has a bug that overrides 
       // era to 64 is eraPeriod equals to zero.
       unsignedTx.era = registry.createType('ImmortalEra').toHex()
+      const decodedUnsignedTx = decodePatch(unsignedTx, txOptions)
       const signingPayload = createSigningPayload(unsignedTx, txOptions);
       const signature = signWith(fromKeypair, signingPayload, txOptions);
       const signedTx = createSignedTx(unsignedTx, signature, txOptions);
       const expectedTxHash = getTxHash(signedTx);
       
-      this.txData.unsignedTx = unsignedTx
+      this.txData.unsignedTx = JSON.stringify(unsignedTx, undefined, 2)
+      this.txData.decodedUnsignedTx = JSON.stringify(decodedUnsignedTx, undefined, 2)
       this.txData.signingPayload = signingPayload
       this.txData.signature = signature
       this.txData.signedTx = signedTx
@@ -183,7 +214,7 @@ export default {
     },
     async submitTx() {
        try {
-        const actualTxHash = await rpcToNode(this.formData.url,'author_submitExtrinsic', [this.txData.signedTx]);
+        const actualTxHash = await rpcToNode(this.broadcastData.url, 'author_submitExtrinsic', [this.broadcastData.signedTx]);
         console.log(`Actual Tx Hash: ${actualTxHash}`);
         this.$buefy.toast.open({
             message: 'Broadcasting transaction was successful!',
@@ -201,7 +232,6 @@ export default {
     resetApp() {
       this.$root.$emit('reset-app')
 
-      this.formData.url = ''
       this.formData.genesisHash = ''
       this.formData.metadata = ''
       this.formData.specVersion = ''
@@ -214,10 +244,14 @@ export default {
       this.formData.mnemonic = '',
 
       this.txData.unsignedTx = ''
+      this.txData.decodedUnsignedTx = ''
       this.txData.signingPayload = ''
       this.txData.signature = ''
       this.txData.signedTx = ''
       this.txData.expectedTxHash = ''
+
+      this.broadcastData.signedTx = ''
+      this.broadcastData.url = ''
 
       this.showForm = true
     }
@@ -225,7 +259,7 @@ export default {
   mounted() {
     this.$root.$on('use-connection', (connectionId) => {
       const connection = getConnection(connectionId)
-      this.formData.url = connection.url
+      this.broadcastData.url = connection.url
       this.formData.genesisHash = connection.genesisHash
       this.formData.metadata = connection.metadata
       this.formData.specVersion = connection.specVersion
